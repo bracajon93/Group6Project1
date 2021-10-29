2c0d8509e58bb3f5bcfcd9c5c273b9859cde6a48
bracajon <65188496+bracajon@users.noreply.github.com>
2021-10-28 22:36:54 -0500

    Update memberList.java

--- Project1Group6/src/Grocery/memberList.java
+++ Project1Group6/src/Grocery/memberList.java
@@ -1,54 +1,16 @@
 package Grocery;
 
-import java.lang.reflect.Member;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.List;
 
-
-public class memberList {
-	Scanner scnr = new Scanner(System.in);
-
-	private static memberList instance = null;
-
-	private ArrayList<Member> listOfMembers = new ArrayList<Member>();
-
-	private memberList(){ /* private constructor for Singleton */
-
+public class MemberList {
+	private static List<Member> memberList = new LinkedList<Member>();
+	
+	public static boolean insertMember(Member member) {
+		memberList.add(member);
+		return true;
+		
 	}
-
-	public static memberList getInstance() {
-        if (instance == null) {
-            instance = new memberList();
-        }
-        return instance;
-    }
-
-	public void addMember() {
-		Member tempMember = new Member();
-		instance.listOfMembers.add(tempMember);
-	}
-
-	public void addMember(String name, String address, int phoneNumber, int dateJoined, Double feePaid) {
-		Member tempMember = new Grocery.Member(name, address, phoneNumber, dateJoined, feePaid);
-		instance.listOfMembers.add(tempMember);
-	}
-
-	public ArrayList<Member> getListOfMembers() {
-		return instance.listOfMembers;
-	}
-
-	/* remove a member when given the id, buisness process 2 */
-	/* does not  seem to require feedback/confirmation, but i put it there anyways*/
-	public void removeMember(int id) {
-		memberList tempInstance = memberList.getInstance();
-		for (Member member : tempInstance.getListOfMembers()) {
-			if (member.getMemberID() == id) {
-				tempInstance.getListOfMembers().remove(member);
-				System.out.println("Member removed.");
-			}
-		}
-	}
-
-
+	
 
 }
